f51346f541910c3b6fb87addad6bc13d6549da76
Siddhant Raghuvanshi <82159869+SudoWodo@users.noreply.github.com>
2026-08-15 00:45:41 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        return f",,".join(strs)
+        
+
+    def decode(self, s: str) -> List[str]:
+        return s.split(',,')
